12593af078ad89aa2f28e01cba41defa15a21e5a
khady ka <dykhaka2@gmail.com>
2026-06-16 23:03:48 +0000

    fix: ajout script creation admin

---
+++ create_admin.py
@@ -0,0 +1,18 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'khadja_shop.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        username='admin',
+        email='admin@khadja.com',
+        password='khadja2024!'
+    )
+    print("✅ Superuser créé !")
+else:
+    print("⚠️ Superuser existe déjà.")
